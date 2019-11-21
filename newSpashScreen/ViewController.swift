c19c4e27e9937f69e420b5070e18515022056b64
Saroj <rawnuk01@gmail.com>
2019-11-20 20:49:46 -0500

    Initial Commit

---
+++ newSpashScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newSpashScreen
+//
+//  Created by Saroj Kunwar on 11/20/19.
+//  Copyright © 2019 Saroj Kunwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
